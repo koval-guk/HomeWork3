7fb006a22aa1e5e372478abd793cc94ca0e1884e
koval-guk <ppp.superkoval@gmail.com>
2022-11-03 23:02:59 +0200

    add Classes

---
+++ src/Employee.java
@@ -0,0 +1,55 @@
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int age;
+
+    public Employee(String name, String position, String email, String phoneNumber, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/packageForSameName/SameName.java
@@ -0,0 +1,4 @@
+package packageForSameName;
+
+public class SameName {
+}
